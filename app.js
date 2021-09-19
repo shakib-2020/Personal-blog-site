7ae1b477fbf1ccb451589100f2d7d4c0e934c752
Md Nazmus Shakib <nazshakib01@gmail.com>
2021-09-19 21:19:18 +0600

    Initial changes and add a Procfile for deployment.

--- app.js
+++ app.js
@@ -354,7 +354,12 @@ app.post("/update/:postId", upload.single("postImage"), async function (
     }
   }
 });
+//post for heroku
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log("Server started on port 3000");
 });
